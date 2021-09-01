02a9ed1c5cb5ebdde6e876cafe39ae0c70e449d0
William <william.oeffner@gmail.com>
2021-08-31 21:02:58 -0400

    Began refactoring code for readability

---
+++ UserMeetTimeClass.py
@@ -0,0 +1,54 @@
+import re
+from datetime import datetime
+from dateutil import tz
+
+validLanguages = ["jp", "en"]
+
+class UserMeetTime:
+    def __init__(self, userInput):
+        self.user = userInput.author.mention
+        self.isValidInput = True
+        self.languageSelected = ""
+        self.desiredDate = datetime.now()
+        self.startTime = datetime.now()
+        self.endTime = datetime.now()
+        self.timezone = ""
+
+
+        content = re.split(' ', userInput.content)
+        self.checkString(content)
+
+    def checkString(self, inputStringArray):
+        if inputStringArray[1] in validLanguages:
+            self.languageSelected = inputStringArray[1]
+        else:
+            self.isValidInput = False
+
+        try:
+            print("input array 2" + inputStringArray[2])
+            date = inputStringArray[2].replace('／', '/')
+
+            baseTime = datetime.strptime(date, '%m/%d')
+
+            self.desiredTime = baseTime.replace(year=datetime.now().year)
+
+            inputStringArray[3].replace('ー', '-')
+
+            times = inputStringArray[3].split('-')
+
+            self.startTime = self.generateTimeAttribute(times[0], self.desiredTime)
+            self.endTime = self.generateTimeAttribute(times[1], self.desiredTime)
+
+            # Need to fix this, timezones require a specific location for tz module
+            self.timezone = inputStringArray[4]
+            # self.timezone = tz.gettz(inputStringArray[4])
+            # self.desiredTime.astimezone(self.timezone)
+        except ValueError:
+            self.isValidInput = False
+
+    def generateTimeAttribute(self, timeStr, dateToReplaceWith):
+        timeStr.replace('：', ':')
+
+        tmpTime = datetime.strptime(timeStr, "%H:%M")
+
+        return tmpTime.replace(year=dateToReplaceWith.year, month=dateToReplaceWith.month, day=dateToReplaceWith.day)
--- main.py
+++ main.py
@@ -1,16 +1,29 @@
+import os
+from dotenv import load_dotenv
+
 import discord
 import re
 import psycopg2
-TOKEN = ''
+from UserMeetTimeClass import UserMeetTime
+load_dotenv()
+TOKEN = os.getenv("DISCORD_TOKEN")
+DB = os.getenv("DB")
+USER = os.getenv("USER")
+PASS = os.getenv("PASS")
+HOST = os.getenv("HOST")
 # 接続に必要なオブジェクトを生成
 
 client = discord.Client()
 def connect():
-    con = psycopg2.connect("")
+    con = psycopg2.connect(host=HOST, database=DB, user=USER, password=PASS)
     return con
+
+
 def exitConnection(con, cur):
     cur.close()
     con.close()
+
+
 def executioner(sql):
     con = connect()
     cur = con.cursor()
@@ -19,41 +32,7 @@ def executioner(sql):
     if not(re.split(' ', sql)[0] == "SELECT"):
         con.commit()
     return (con, cur)
-def timeEncoder(s, tz, dt):
-    a = re.split('[:：]', s)
-    i = int(a[0]) * 60 + int(a[1]) - int(tz) * 60
-    if i < 0:
-        i = 60 * 24 + i
-        s = re.split('[/／]', dt)
-        s[1] = str(int(s[1]) - 1)
-        realDt = s[0] + '/' + s[1]
-        return i, realDt
-    elif i >= 60 * 24:
-        i = i - 60 * 24
-        s = re.split('[/／]', dt)
-        s[1] = str(int(s[1]) + 1)
-        realDt = s[0] + '/' + s[1]
-        return i, realDt
-    return i, dt
-def timeDecoder(x, y, tz, dt):
-    a = x + 60 * tz
-    b = y + 60 * tz
-    realDt = dt
-    s = re.split('[/／]', dt)
-    if a >= 60 * 24:
-        a = a - 60 * 24
-        s[1] = str(int(s[1]) + 1)
-    elif a < 0:
-        a = 60 * 24 + a
-        s[1] = str(int(s[1]) - 1)
-    if b >= 60 * 24:
-        b = b - 60 * 24
-    elif b < 0:
-        b = 60 * 24 + b
-    realDt = s[0] + '/' + s[1]
-    start = str(a // 60) + ':' + str(a % 60)
-    end = str(b // 60) + ':' + str(b % 60)
-    return start, end, realDt
+
 def checkTime(startTime, endTime, secondStartTime, secondEndTime):
     first = set(range(startTime, endTime))
     second = set(range(secondStartTime, secondEndTime))
@@ -64,36 +43,30 @@ def checkTime(startTime, endTime, secondStartTime, secondEndTime):
         inter = list(inter)
         inter.sort()
         return inter[0], inter[-1] + 1
-def register(s):
-    dt = s[2]
-    dt = formStringMonth(dt)
-    start, end = re.split('[-ー]', s[3])
-    x = timeEncoder(start, s[4], dt)
-    y = timeEncoder(end, s[4], dt)
-    print(x)
-    print(y)
 
-    sql = "INSERT INTO USERS (name, timezone, lang, startTime, endTime, firstDate, secondDate) VALUES ('%s', %s, '%s', '%s', '%s', '%s', '%s')" % (s[0], s[4], s[1], x[0], y[0], x[1], y[1])
+
+def register(userInput):
+    sql = "INSERT INTO USERS (name, timezone, lang, startTime, endTime, firstDate, secondDate) VALUES ('%s', %s, '%s', '%s', '%s', '%s', '%s')" % (userInput.user, 0, userInput.languageSelected, userInput.startTime, userInput.endTime, userInput.desiredDate, userInput.desiredDate)
     exitConnection(*executioner(sql))
     print("Registered")
+
 
 def init():
     sql = "CREATE TABLE USERS (id SERIAL, name text, timezone integer, lang text, startTime text, endTime text, firstDate text, secondDate text)"
     exitConnection(*executioner(sql))
     print("Initialized")
-def checkString(s):
-    if s[1] == 'jp' or s[1] == 'en':
-        if '/' in s[2] or '／' in s[2]:
-            t = re.split('[-ー]', s[3])
-            if (':' in t[0] and ':' in t[1]) or ('：' in t[0] and '：' in t[1]):
-                if -12 <= int(s[4]) and int(s[4]) <= 12:
-                    return True
+
+
+
+
 def checkID(name):
     sql = "SELECT id from USERS WHERE name='%s'" % name
     con, cur = executioner(sql)
     id = cur.fetchone()[0]
     exitConnection(con, cur)
     return id
+
+
 def match(id):
     sql = "SELECT name, timezone, lang, startTime, endTime, firstDate, secondDate  from USERS WHERE id=%s" % id
     con, cur = executioner(sql)
@@ -117,6 +90,8 @@ def match(id):
                         exitConnection(*executioner(sql))
                         return mes 
     return "No one was matched to you for now."
+
+
 def cancel(name, id):
     sql = "SELECT name FROM USERS WHERE id=%s" % id
     con, cur = executioner(sql)
@@ -130,6 +105,8 @@ def cancel(name, id):
             return "You can't cancel other's reservation"
     except TypeError:
         return "You don't have any reservation"
+
+
 def formStringTime(s):
     st = re.split('[:：]', s)
     if (int(st[0]) < 10 and not("0" in st[0])) or st[0] == "0":
@@ -137,6 +114,8 @@ def formStringTime(s):
     if (int(st[1]) < 10 and not("0" in st[1])) or st[1] == "0":
         st[1] = '0' + st[1]
     return st[0] + ':' + st[1]
+
+
 def formStringMonth(s):
     st = re.split('[/／]', s)
     if (int(st[0]) < 10 and not("0" in st[0])) or st[0] == "0":
@@ -144,6 +123,8 @@ def formStringMonth(s):
     if (int(st[1]) < 10 and not("0" in st[1]))  or st[1] == "0":
         st[1] = '0' + st[1]
     return st[0] + '/' + st[1] 
+
+
 def showAllUsers():
     sql = "SELECT * FROM USERS"
     con, cur = executioner(sql)
@@ -152,52 +133,59 @@ def showAllUsers():
     for r in rows:
         print(r)
     exitConnection(con, cur)
+
+
 def showChannels(g):
     s = ""
     for v in g.voice_channels:
         s += (v.name + "\n")
     return s
+
+
 @client.event
 async def on_ready():
     # 起動したらターミナルにログイン通知が表示される
     print('ログインしました')
 # メッセージ受信時に動作する処理
 @client.event
+
+
 async def on_message(message):
     # メッセージ送信者がBotだった場合は無視する
     if message.author.bot:
         return
     if message.content.startswith('/match'):
-        # /join jp(en) 09/22 21:00-22:00 9
-        s = re.split(' ', message.content)
-        s[0] = message.author.mention
-        if checkString(s):
-            register(s)
-            id = checkID(message.author.mention)
-            await message.channel.send('Registered ' + message.author.display_name + " Your ID is " + str(id))
-            m = match(id)
-            if "No one" in m:
-                await message.channel.send(m)
-            else:
-                print(m)
-                mes = m[0] + " Your language exchange starts at " + formStringTime(m[1][0]) + " , and ends at " + formStringTime(m[1][1]) + " on " + m[1][2]
-                await message.channel.send(mes)
-                mes = m[2] + " Your language exchange starts at " + formStringTime(m[3][0]) + " , and ends at " + formStringTime(m[3][1]) + " on " + m[3][2]
-                await message.channel.send(mes)
-                guild = client.get_guild(message.guild.id)
-                await message.channel.send(showChannels(guild) + "are voice channels that you can use.")
-        else:
-            await message.channel.send('Wrong Command. Please check your command)')
-    elif message.content.startswith('/cancel'):
-        id = re.split(' ', message.content)[1]
-        await message.channel.send(cancel(message.author.mention, id))
-    elif message.content.startswith('/init'):
-        init()
-        await message.channel.send('Initialized Database')
-    elif message.content.startswith('/users'):
-        await message.channel.send(showAllUsers())
-    elif message.content.startswith('/channels'):
-        guild = client.get_guild(message.guild.id)
-        await message.channel.send(showChannels(guild) + "are voice channels that you can use.")
+        # /match jp(en) 09/22 21:00-22:00 9
+        userInput = UserMeetTime(message)
+        if userInput.isValidInput:
+            register(userInput)
+    #         id = checkID(message.author.mention)
+    #         await message.channel.send('Registered ' + message.author.display_name + " Your ID is " + str(id))
+    #         m = match(id)
+    #         if "No one" in m:
+    #             await message.channel.send(m)
+    #         else:
+    #             print(m)
+    #             mes = m[0] + " Your language exchange starts at " + formStringTime(m[1][0]) + " , and ends at " + formStringTime(m[1][1]) + " on " + m[1][2]
+    #             await message.channel.send(mes)
+    #             mes = m[2] + " Your language exchange starts at " + formStringTime(m[3][0]) + " , and ends at " + formStringTime(m[3][1]) + " on " + m[3][2]
+    #             await message.channel.send(mes)
+    #             guild = client.get_guild(message.guild.id)
+    #             await message.channel.send(showChannels(guild) + "are voice channels that you can use.")
+    #     else:
+    #         await message.channel.send('Wrong Command. Please check your command)')
+    # elif message.content.startswith('/cancel'):
+    #     id = re.split(' ', message.content)[1]
+    #     await message.channel.send(cancel(message.author.mention, id))
+    # elif message.content.startswith('/init'):
+    #     init()
+    #     await message.channel.send('Initialized Database')
+    # elif message.content.startswith('/users'):
+    #     await message.channel.send(showAllUsers())
+    # elif message.content.startswith('/channels'):
+    #     guild = client.get_guild(message.guild.id)
+    #     await message.channel.send(showChannels(guild) + "are voice channels that you can use.")
+
+
 # Botの起動とDiscordサーバーへの接続
 client.run(TOKEN)
